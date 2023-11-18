baea0c6d5aebd6815f1d32c0c293e7e2c9d25b0e
Victor Vasconcelos <victorguilhermecavalcante@gmail.com>
2023-11-17 20:15:59 -0400

    feat: create components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  standalone: false,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'first-app';
+  helloWorld = 'olá mundo';
 }
